9f282f968f5bfdc844796ec1dcce1046b552d3fe
Caue Queiroz <cauenqueiroz@gmail.com>
2020-07-10 20:42:05 -0300

    First commit

---
+++ week1/program1.py
@@ -0,0 +1,16 @@
+x = int(input('Enter x: '))
+y = int(input('Enter y: '))
+
+print('')
+print('Processing...')
+print('')
+
+if x == y:
+    print('The numbers are the same.')
+elif x > y:
+    print('x is greater than y.')
+else: 
+    print('x is smaller than y.')
+
+print('')
+print('End of the program.')
